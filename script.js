0483806f5634a01feb19b98e6ff4a3bc2f8ba452
Aman Singh <aman26092000@gmail.com>
2021-09-11 03:31:12 +0530

    Completed Notes Web App

--- script.js
+++ script.js
@@ -1,9 +1,107 @@
+alert("Open the Database!!")
 
-
+let view=document.querySelector("#view");
 let openBtn=document.querySelector("#open");
 let addBtn=document.querySelector("#add");
 let input=document.querySelector("input");
+let body=document.querySelector("body");
+let table=document.querySelector("table");
 let db;
+
+// let tempData = [
+//     { cId: 2423534534, note: "this is note 1" },
+//     { cId: 2426634534, note: "this is note2" },
+//     { cId: 2113534534, note: "this is note 3" },
+//   ];
+
+view.addEventListener("click",function(){
+
+    let isOpen=view.getAttribute("data-open");
+    if(isOpen=="true"){
+        table.innerHTML="";
+        view.setAttribute("data-open","false");
+        
+        return;
+    }
+    view.setAttribute("data-open","true");
+    table.innerHTML=`<table>
+    <thead>
+        <th>Sr No.</th>
+        <th>Note</th>
+        <th>Delete</th>
+    </thead>
+    <tbody>
+    </tbody>`;
+
+    let tx=db.transaction("csNotes", "readonly");
+
+    let csNotesObjectStore=tx.objectStore("csNotes");
+
+    let tbody=table.querySelector("tbody");
+
+    let serialNumber=1;
+
+    let req=csNotesObjectStore.openCursor();
+    req.addEventListener("success",function(){
+        let cursor=req.result;
+
+        if(cursor){
+            let currObj=cursor.value;
+            // console.log(currObj);
+            let tr=document.createElement("tr");
+            tr.innerHTML=`<td>${serialNumber}</td>
+            <td>${currObj.note}</td>
+            <td><button data-id=${currObj.cId}>Delete</button></td>`;
+            let deleteBtn=tr.querySelector("button");
+            deleteBtn.addEventListener("click",function(){
+                deleteBtn.parentElement.parentElement.remove();
+                deleteNote(Number(deleteBtn.getAttribute("data-id")));
+            })
+            
+            tbody.append(tr);
+
+            serialNumber++;
+
+            
+
+            cursor.continue();
+            
+        }
+
+    })
+
+
+
+    
+    // let tbody=document.querySelector("tbody");
+    // for(let i=0;i<tempData.length;i++){
+    //     let tr=document.createElement("tr");
+    //     tr.innerHTML=`<td>${i+1}</td>
+    //     <td>${tempData[i].note}</td>
+    //     <td><button>Delete</button></td>`;
+    //     tbody.append(tr);
+    // }
+})
+
+  
+
+// view.addEventListener("click",function(){
+//     for(let i=0;i<tempData.length;i++){
+//         let cuurentData=tempData[i];
+//         let div=document.createElement('div');
+//         div.classList.add("note");
+//         div.innerHTML=`<div class="id-div"></div>
+//         <div class="note-div"></div>`
+
+//         let idDiv=div.querySelector(".id-div");
+//         let noteDiv=div.querySelector(".note-div");
+//         idDiv.innerText=cuurentData.cId;
+//         noteDiv.innerText=cuurentData.note;
+//         div.append(idDiv);
+//         div.append(noteDiv);
+//         body.append(div);
+//     }
+// })
 
 addBtn.addEventListener("click",function(){
     if(!db){
@@ -37,7 +135,7 @@ openBtn.addEventListener("click",function(){
     res.addEventListener("upgradeneeded",function(){
         db=res.result;
         db.createObjectStore("csNotes", { keyPath:"cId" });
-        console.log(2);/
+        console.log(2);
         // console.log(db);
     
     })
@@ -46,5 +144,17 @@ openBtn.addEventListener("click",function(){
         alert("Error occurred while opening the Database");
     })
 })
+
+
+function deleteNote(cId){
+    let tx=db.transaction("csNotes","readwrite");
+    let csNotesObjectStore=tx.objectStore("csNotes");
+    csNotesObjectStore.delete(cId);
+    let isOpen=view.getAttribute("data-open");
+    if(isOpen=="true"){
+        view.click();
+        view.click();
+    }
+}
 
 
